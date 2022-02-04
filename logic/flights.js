a6a11b4d68802e1805806de64ae56f8e596a3ef1
Jeff Constantine <conno80@gmail.com>
2022-02-04 11:54:32 +0800

    Update flights.js
    added error code to restrict number of passengers

--- logic/flights.js
+++ logic/flights.js
@@ -1,23 +1,28 @@
 function Flights() {
-    function calculateNumberOfFlights(passengerNo, flightCapacity) {
-        let flights;
+  function calculateNumberOfFlights(passengers, capacity) {
+    let flights;
 
-        if (passengerNo % flightCapacity == 0) {
-            flights = passengerNo/flightCapacity;
-        } else {
-            flights = Math.floor(passengerNo/flightCapacity) + 1;
-        }
-        return flights; 
-    }          
-    
-    return {calculateNumberOfFlights};
+    if (passengers < 0 || !Number.isInteger(Number(passengers))) {
+      throw new Error(
+        "The number of passengers must be a positive integer value"
+      );
+    }
+
+    if (capacity < 0 || !Number.isInteger(Number(capacity))) {
+      throw new Error(
+        "The capacity of the flight must be a positive integer value"
+      );
+    }
+
+    if (passengerNo % flightCapacity == 0) {
+      flights = passengerNo / flightCapacity;
+    } else {
+      flights = Math.floor(passengerNo / flightCapacity) + 1;
+    }
+    return flights;
+  }
+
+  return { calculateNumberOfFlights };
 }
 
-
-
-
-
-
-
 module.exports = Flights();
-
